6d6f7e1927953c535ccca89eff6713bdacbb50e8
Moctar <moctarjallo@gmail.com>
2024-11-20 20:34:40 +0000

    Update manifests to 17.0

--- hotel_housekeeping/__manifest__.py
+++ hotel_housekeeping/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Hotel Housekeeping Management",
-    "version": "15.0.1.0.0",
+    "version": "17.0.1.0.0",
     "author": "Odoo Community Association (OCA), Serpent Consulting \
                Services Pvt. Ltd., Odoo S.A.",
     "website": "https://github.com/OCA/vertical-hotel",
